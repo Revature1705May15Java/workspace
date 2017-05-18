46364a8f5a33e009349f692d266e19a543d0932c
Cody Boucher <Cody Boucher>
2017-05-18 17:15:16 -0400

    add Exercises

--- Cody_Boucher_Code/Exercises/src/com/ex/threads/MyRunnable.java
+++ Cody_Boucher_Code/Exercises/src/com/ex/threads/MyRunnable.java
@@ -2,9 +2,23 @@ package com.ex.threads;
 
 public class MyRunnable implements Runnable{
 	
-	StringBuffer sb = new StringBuffer("runnable");
+	//StringBuffer sb = new StringBuffer("runnable");
+	StringBuilder sb = new StringBuilder("runnable");
+	
 	@Override
 	public void run() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(sb.append(i));
+		}
+	}
+	
+	public void run(StringBuilder sb) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(sb.append(i));
+		}
+	}
+	
+	public void run(StringBuffer sb) {
 		for(int i = 0; i < 10; i++) {
 			System.out.println(sb.append(i));
 		}
--- Cody_Boucher_Code/Exercises/src/com/rev/lambdas/LambdaExample.java
+++ Cody_Boucher_Code/Exercises/src/com/rev/lambdas/LambdaExample.java
@@ -3,9 +3,25 @@ package com.rev.lambdas;
 public class LambdaExample {
 
 	public static void main(String[] args) {
-		LambdaExample ex = new LambdaExample();
 		
+		String name = "Boucher";
+		stringInfo nameInfo = (String str) -> str.length();
+		int getLength = nameInfo.stringLength(name);
+		System.out.println("String Length of Boucher is: " + getLength);
+		
+		int num = 5;
+		numberTest numInfo = (int i) -> i % 2 == 0;
+		boolean getNum = numInfo.isEven(num);
+		System.out.println("Is The Number " + num + " Even: " + getNum);
 
 	}
+	
+}
 
+interface stringInfo {
+	int stringLength(String str);
+}
+
+interface numberTest {
+	boolean isEven(int i);
 }
