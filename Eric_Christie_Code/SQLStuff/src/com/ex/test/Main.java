74f1ece260844d5515456201212f0d5baf03d90b
Eric Christie <ericjchristie@gmail.com>
2017-05-24 17:45:50 -0400

    changed all tabs to 2 spaces

--- Eric_Christie_Code/SQLStuff/src/com/ex/test/Main.java
+++ Eric_Christie_Code/SQLStuff/src/com/ex/test/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		ImplementedDAO dao = new ImplementedDAO();
-//		dao.addOrder(2, 50.00);
-//		dao.addCustomer("person", "one", "GA", 2.0, "1@email.com");
+		// dao.addOrder(2, 50.00);
+		// dao.addCustomer("person", "one", "GA", 2.0, "1@email.com");
 		Customer temp = dao.getCustomer(1);
 		System.out.println(temp);
-//		System.out.println(dao.getAllCustomers());
-		
-//		dao.createOrder(temp, 24.95);
-		
+		// System.out.println(dao.getAllCustomers());
+
+		// dao.createOrder(temp, 24.95);
+
 		ArrayList<Order> orders = dao.getOrdersByCustomer(temp);
 		System.out.println(orders);
 	}
