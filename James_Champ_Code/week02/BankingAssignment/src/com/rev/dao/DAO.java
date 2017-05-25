e8f8e2f524f0dcb6ccdfbb9a7973507ade2fbcf7
James Champ <jameschamp@acm.org>
2017-05-25 15:46:51 -0400

    Added methods to DAO interface.

--- James_Champ_Code/week02/BankingAssignment/src/com/rev/dao/DAO.java
+++ James_Champ_Code/week02/BankingAssignment/src/com/rev/dao/DAO.java
@@ -3,5 +3,6 @@ package com.rev.dao;
 import com.rev.pojos.Account;
 
 public interface DAO {
-	public int updateBalance(Account account);
+	public int updateBalance(Account account, double newBalance);
+	public int addUser(String firstName, String lastName, String email, String password);
 }
