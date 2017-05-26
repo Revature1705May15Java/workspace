3d8b0cbd726acf9b526dc49e64f9adb75d0c7269
Eric <Eric@eric-pc>
2017-05-26 19:52:39 -0400

    more git in eclipse

---
+++ Eric_Christie_Code/BankingApp/src/com/ex/util/ConnectionUtil.java
@@ -0,0 +1,18 @@
+package com.ex.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+  public static Connection getConnection() throws SQLException {
+    String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    String username = "bankdb";
+    String password = "bankdbpass";
+
+    return DriverManager.getConnection(url, username, password);
+  }
+
+}
+
