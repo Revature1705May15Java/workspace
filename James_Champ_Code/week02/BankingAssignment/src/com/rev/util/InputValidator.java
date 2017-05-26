f0d23ed02b0918ba3f7d83489cda28159a785790
James Champ <jameschamp@acm.org>
2017-05-26 09:08:22 -0400

    Added email validation
    Created method that determines whether an email address is properly
    formed.

--- James_Champ_Code/week02/BankingAssignment/src/com/rev/util/InputValidator.java
+++ James_Champ_Code/week02/BankingAssignment/src/com/rev/util/InputValidator.java
@@ -1,5 +1,27 @@
 package com.rev.util;
 
-public class InputValidator {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class InputValidator {
+	/**
+	 * Regular expression used to validate that email addresses are properly formatted.
+	 */
+	private static final String EMAIL_PATTERN =
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
+	/**
+	 * Checks the given String to see if it is a properly formed email address.
+	 * 
+	 * @param email	The String that is to be tested.
+	 * @return	{@code true} if the given String is a properly formatted email 
+	 * 			address, otherwise {@code false}.
+	 */
+	public static boolean validateEmail(String address) {
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcher = pattern.matcher(address);
+		
+		return matcher.matches();
+	}
 }
