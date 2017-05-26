6aa09f432f7d56946c327fffa1b3365cc668eed9
James Champ <jameschamp@acm.org>
2017-05-25 20:09:28 -0400

    Began creating front-end
    Started creating menu that users will use to interact
    with their accounts.

--- James_Champ_Code/week02/BankingAssignment/src/com/rev/main/Main.java
+++ James_Champ_Code/week02/BankingAssignment/src/com/rev/main/Main.java
@@ -1,12 +1,85 @@
 package com.rev.main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	private static final String BANK_NAME = "Placeholder Bank Name";
+	private static final int INVALID_SELECTION = -1;
+	
+	private static Scanner scan;
+	
 	public static void main(String[] args) {
 		run();
 	}
 
 	public static void run() {
+		scan = new Scanner(System.in);
 		
+		System.out.println("\tWelcome to " + BANK_NAME + " portal.\n");
+		
+		while(true) {
+			System.out.println(printInitialOptions());
+		}
+	}
+	
+	// TODO: Add ability to exit program.
+	private static int printInitialOptions() {
+		int result;
+		String line;
+		
+		System.out.println("\tWhat would you like to do?\n");
+
+		do {
+			
+			System.out.println("\t1. Log in to existing account.");
+			System.out.println("\t2. Become a member.\n");
+			
+			System.out.print("\tMake a selection [1 - 2]: ");
+			
+			line = scan.nextLine();
+			System.out.println();
+			
+			try {
+				result = Integer.parseInt(line);
+				
+				if(result < 1 || result > 2) {
+					result = INVALID_SELECTION;
+					printErrorMessage(2);
+				}
+			}
+			catch(NumberFormatException e) {
+				result = INVALID_SELECTION;
+				printErrorMessage(2);
+			}
+			
+
+		} while(result == INVALID_SELECTION);
+		
+		return result;
+	}
+	// Prompt for account holder
+	
+	// If no:
+		// Sign up (Logged in after successful sign-up)
+			// Create account
+	// Else:
+		// List accounts
+		// View account details (List account holders)
+			// Choose account if more than one
+				// Check balance
+				// Deposit
+				// Withdraw
+				// Transfer funds
+				// Close account
+				// Add account holder
+		// Create new account
+			// Is joint account
+			// Account type
+		// Log out
+	
+	private static void printErrorMessage(int totalSelections) {
+		System.out.println("\n\tInvalid selection.\n" +
+						 	"\tPlease enter a whole number between 1 and " +
+							totalSelections + ".\n");
 	}
 }
