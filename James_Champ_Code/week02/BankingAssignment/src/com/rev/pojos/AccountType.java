e510439014f5bd595b590a0e16f9b248054511aa
James Champ <jameschamp@acm.org>
2017-05-26 13:08:24 -0400

    Created AccountType constructor
    Also added getName() method.

--- James_Champ_Code/week02/BankingAssignment/src/com/rev/pojos/AccountType.java
+++ James_Champ_Code/week02/BankingAssignment/src/com/rev/pojos/AccountType.java
@@ -1,6 +1,30 @@
 package com.rev.pojos;
 
 public class AccountType {
+	public static final int CHECKING = 1;
+	public static final int SAVINGS = 2;
+	public static final int CREDIT = 3;
+	
 	private int typeId;
 	private String name;
+	
+	public AccountType(int type) {
+		typeId = type;
+		
+		switch(typeId) {
+			case CHECKING:
+				name = "Checking";
+				break;
+			case SAVINGS:
+				name = "Savings";
+				break;
+			case CREDIT:
+				name = "Credit";
+				break;
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
