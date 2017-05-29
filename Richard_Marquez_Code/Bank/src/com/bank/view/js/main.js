339149b5dab88942dfd2bb23ad8820252245af29
Richard Marquez <richard92m@me.com>
2017-05-29 15:32:25 -0400

    user stories styled

--- Richard_Marquez_Code/Bank/src/com/bank/view/js/main.js
+++ Richard_Marquez_Code/Bank/src/com/bank/view/js/main.js
@@ -1,41 +1,52 @@
-$(document).ready(function() {
-  initScrollWatcher();
-  //initSections();
+$(document).ready(function () {
+    initScrollWatcher();
+    initSmoothScroll();
 });
 
+// FROM W3SCHOOLS
+function initSmoothScroll() {
+// Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+}
+
 function initScrollWatcher() {
-  $(window).scroll(function() {
-    if ($(window).scrollTop() <= 20) {
-      $('nav').css('opacity', '0');
-    } else {
-      $('nav').css('opacity', '1');
-    }
-  });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() <= 20) {
+            $('nav').css('opacity', '0');
+        } else {
+            $('nav').css('opacity', '1');
+        }
+    });
 }
 
-function initSections() {
-  $('section').each(function() {
-      //$(this).css('background-color', getRandomColor()+""+"ff");
-      $(this).css('background-color', getRandomRGBA(.18));
-  });
-}
-
-function getRandomRGBA(alpha) {
-    var rand1 = Math.floor(Math.random() * 200) + 55;
-    var rand2 = Math.floor(Math.random() * 200) + 55;
-    var rand3 = Math.floor(Math.random() * 200) + 55;
-    var rgba = 'rgba(' + rand1 + ',' + rand2 + ',' + rand3 + ',' + alpha + ')';
-
-    return rgba;
-}
 // Get random 6-digit hex color e.g. "#a82df1"
 function getRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
 
-  for (var i = 0; i < 6; i++ ) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
 
-  return color;
+    return color;
 }
