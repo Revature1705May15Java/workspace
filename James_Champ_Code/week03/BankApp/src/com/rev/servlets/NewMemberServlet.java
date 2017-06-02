58a2cc2c437c8b36150d081a42a81bba0ed91766
James Champ <jameschamp@acm.org>
2017-06-02 10:05:14 -0400

    New users can now be created
    Website now allows users to become bank members. New users are added to
    the database.

--- James_Champ_Code/week03/BankApp/src/com/rev/servlets/NewMemberServlet.java
+++ James_Champ_Code/week03/BankApp/src/com/rev/servlets/NewMemberServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,27 +12,40 @@ import javax.servlet.http.HttpServletResponse;
 import com.rev.pojos.User;
 import com.rev.service.Service;
 
-public class LoginServlet extends HttpServlet {
+/**
+ * Servlet implementation class NewMemberServlet
+ */
+@WebServlet("/NewMemberServlet")
+public class NewMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("username");
+		String firstName = request.getParameter("fname");
+		String lastName = request.getParameter("lname");
+		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		
 		Service service = new Service();
+		User user = service.addUser(firstName, lastName, password, email);
 		
-		User u = service.getUser(email);
-		if(password.equals(u.getPassword())){
-			RequestDispatcher rd = request.getRequestDispatcher("success.html");
-			rd.forward(request, response);
-		}
-		else {
+		if(user == null) {
 			RequestDispatcher rd = request.getRequestDispatcher("error.html");
 			rd.forward(request, response);
 		}
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("success.html");
+			rd.forward(request, response);
+		}
 	}
+
 }
