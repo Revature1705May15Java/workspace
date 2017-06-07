580d99dfa38740c5393b7f5248580c2149de6d9b
M <tmdinh1201@gmail.com>
2017-06-07 10:50:57 -0400

    miscellany update and index pdf add

--- Theresa_Dinh_Code/Week4/JSPractice/WebContent/JavaScript/examples.js
+++ Theresa_Dinh_Code/Week4/JSPractice/WebContent/JavaScript/examples.js
@@ -1,30 +1,61 @@
 /**
  * 
  */
-function fib(n){
-	var num = 0;
-	var num2 = 1; 
-	var temp = 1; 
-	for(i = 0; i < n-2; i++)
-	{
-		num += num2; 
-		num2 = temp; 
-		temp = num; 
-	}
-	return num;
+//function fib(n){
+//	var num = 0;
+//	var num2 = 1; 
+//	var temp = 1; 
+//	for(i = 0; i < n-2; i++)
+//	{
+//		num += num2; 
+//		num2 = temp; 
+//		temp = num; 
+//	}
+//	return num;
+//}
+
+function add()
+{
+	var id = document.getElementById("t_id").value;
+	var name = document.getElementById("t_name").value;
+	var major = document.getElementById("t_major").value; 
+	
+	var row = document.createElement("tr"); 
+	
+	var cell = document.createElement("td"); 
+	var cell2 = document.createElement("td");
+	var cell3 = document.createElement("td");
+	
+	row.appendChild(cell); 
+	row.appendChild(cell2); 
+	row.appendChild(cell3); 
+	
+	cell.innerHTML = id; 
+	cell2.innerHTML = name; 
+	cell3.innerHTML = major; 
+	
+	document.getElementById("students").appendChild(row); 
 }
 
-elem = document.getElementById("fib"); 
-elem.innerHTML=fib(4);
+document.getElementById("add").addEventListener("click", add, false); 
 
-var parags = document.getElementsByTagName("p"); 
-//var inner;
-//for(i = 0; i < parags.length; i++)
-//	{
-//		inner.push(parags[i].innerHTML); 
-//	}
+function count()
+{	
+	
+}
 
-document.getElementById("display").innerHTML = parags[0].innerHTML;
+document.getElementbyId("add").addEventListener("click", count, false); 
+
+
+
+//
+//elem = document.getElementById("fib"); 
+//elem.innerHTML=fib(4);
+//
+//var parags = document.getElementsByTagName("p"); 
+
+
+//document.getElementById("display").innerHTML = parags[0].innerHTML;
 
 //var x = fib(5); 
 //elem.appendChild(x); 
--- Theresa_Dinh_Code/Week4/JSPractice/WebContent/JavaScript/homework.js
+++ Theresa_Dinh_Code/Week4/JSPractice/WebContent/JavaScript/homework.js
@@ -1,8 +1,5 @@
-/**
- * 
- */
-
-function fib(n){
+function fib(n)
+{
 	var num = 0;
 	var num2 = 1; 
 	var temp = 1; 
@@ -15,10 +12,19 @@ function fib(n){
 	return num;
 }
 
+function test()
+{
+    var input = document.getElementById("input").value; 
+    var answer = document.getElementById("answer"); 
+    answer.innerHTML = fib(input); 
+//    change function above ^ as needed
+}
+document.getElementById("tester").addEventListener("click", test, false);        
+    
 function reverseStr(someStr)
 {
 	var str = "";
-	var array = someStr.split(""); 
+	var array = someStr.split("");
 	for(var s in array)
 		str += array[s]
 	return str; 
